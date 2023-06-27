51c09c9a7ae5a1d147ddca9b9841d55e601a85f3
kiran-c8 <kiranc8298@gmail.com>
2023-06-27 18:19:36 +0530

    init commit

---
+++ src/components/TodoItem/TodoItem.js
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+import Checkbox from "@mui/material/Checkbox";
+import IconButton from "@mui/material/IconButton";
+import DeleteIcon from "@mui/icons-material/Delete";
+import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
+import RadioButtonUncheckedOutlinedIcon from "@mui/icons-material/RadioButtonUncheckedOutlined";
+import TextField from "@mui/material/TextField";
+
+const TodoItem = ({
+  todo,
+  handleCheckboxChange,
+  handleDeleteTodo,
+  handleEditTodo,
+}) => {
+  const [isEditing, setIsEditing] = useState(false); // State to track if the todo item is being edited
+  const [updatedTitle, setUpdatedTitle] = useState(todo.title); // State to store the updated title of the todo item
+
+  const handleCheckboxClick = () => {
+    handleCheckboxChange(todo.id); // Call the parent function to handle checkbox click
+  };
+  const handleInputChange = (e) => {
+    setUpdatedTitle(e.target.value); // Update the updated title state as the input value changes
+  };
+  const handleInputBlur = () => {
+    setIsEditing(false); // Disable editing mode
+    handleEditTodo(todo.id, updatedTitle); // Call the parent function to handle todo item edit
+  };
+  const handleDeleteClick = () => {
+    handleDeleteTodo(todo.id, updatedTitle); // Call the parent function to handle todo item deletion
+  };
+
+  const handleDoubleClick = () => {
+    setIsEditing(true); // Enable editing mode on double-click
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        marginBottom: "8px",
+        backgroundColor: todo.completed ? "#eaf7ea" : "#fff",
+        padding: "8px",
+        borderRadius: "4px",
+      }}
+    >
+      <Checkbox
+        checked={todo.completed}
+        onChange={handleCheckboxClick}
+        icon={<RadioButtonUncheckedOutlinedIcon />} // Icon to display when the checkbox is unchecked
+        checkedIcon={<CheckCircleOutlineIcon style={{ color: "green" }} />} // Icon to display when the checkbox is checked
+      />
+      {isEditing ? (
+        <TextField
+          fullWidth
+          value={updatedTitle}
+          onBlur={handleInputBlur}
+          onChange={handleInputChange}
+          autoFocus
+          InputProps={{
+            style: { border: "1px solid lightgray" },
+          }}
+        />
+      ) : (
+        <div onDoubleClick={handleDoubleClick} style={{ flexGrow: 1 }}>
+          {todo.title}
+        </div>
+      )}
+      <IconButton aria-label="delete" onClick={handleDeleteClick}>
+        <DeleteIcon style={{ color: "red" }} />
+      </IconButton>
+    </div>
+  );
+};
+
+export default TodoItem;
---
+++ src/components/TodoList/Todo.js
@@ -0,0 +1,176 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import InputBase from "@mui/material/InputBase";
+import Divider from "@mui/material/Divider";
+import AddIcon from "@mui/icons-material/Add";
+import TodoItem from "../TodoItem/TodoItem";
+
+const Todo = () => {
+  const [todos, setTodos] = useState([]); // State to store the todos
+  const [newTodo, setNewTodo] = useState(""); // State to track the value of the new todo input field
+  const [pendingTasks, setPendingTasks] = useState(0); // State to track the number of pending tasks
+
+  useEffect(() => {
+    // Fetch initial todos from an API endpoint (using jsonplaceholder as an example)
+    axios.get("https://jsonplaceholder.typicode.com/todos").then((res) => {
+      setTodos(res.data); // Set the fetched todos in state
+      setPendingTasks(res.data.filter((todo) => !todo.completed).length); // Calculate the number of pending tasks
+    });
+  }, []);
+
+  const handleInputChange = (e) => {
+    // Handler for new todo input field change
+    setNewTodo(e.target.value); // Update the new todo value
+  };
+
+  const handleAddTodo = () => {
+    // Handler for adding a new todo
+    const newTodoItem = {
+      id: todos.length + 1,
+      title: newTodo,
+      completed: false,
+    };
+    axios
+      .post("https://jsonplaceholder.typicode.com/todos", newTodoItem)
+      .then((res) => {
+        const addedTodo = res.data;
+        setTodos([...todos, addedTodo]); // Add the new todo to the todos state
+        setNewTodo(""); // Clear the new todo input field
+        setPendingTasks(pendingTasks + 1); // Increment the number of pending tasks
+      })
+      .catch((error) => console.log(error));
+  };
+
+  const handleCheckboxChange = (todoId) => {
+    // Handler for checkbox toggle
+    const updatedTodos = todos.map((todo) => {
+      if (todo.id === todoId) {
+        const updatedTodo = { ...todo, completed: !todo.completed }; // Toggle the completed status
+        axios
+          .put(
+            `https://jsonplaceholder.typicode.com/todos/${todoId}`,
+            updatedTodo
+          )
+          .then(() => {
+            setTodos((prevTodos) =>
+              prevTodos.map((prevTodo) =>
+                prevTodo.id === todoId ? updatedTodo : prevTodo
+              )
+            );
+          })
+          .catch((error) => console.log(error));
+      }
+      return todo;
+    });
+    setTodos(updatedTodos); // Update the todos state with the updated list
+    setPendingTasks(updatedTodos.filter((todo) => !todo.completed).length); // Update the number of pending tasks
+  };
+
+  const handleEditTodo = (todoId, updatedTitle) => {
+    // Handler for editing a todo
+    const updatedTodos = todos.map((todo) => {
+      if (todo.id === todoId) {
+        const updatedTodo = { ...todo, title: updatedTitle };
+        axios
+          .put(
+            `https://jsonplaceholder.typicode.com/todos/${todoId}`,
+            updatedTodo
+          )
+          .then(() => {
+            setTodos((prevTodos) =>
+              prevTodos.map((prevTodo) =>
+                prevTodo.id === todoId ? updatedTodo : prevTodo
+              )
+            );
+          })
+          .catch((error) => console.log(error));
+        // return updatedTodo;
+      }
+      return todo;
+    });
+    setTodos(updatedTodos); // Update the todos state
+  };
+
+  const handleDeleteTodo = (todoId) => {
+    // Handler for deleting a todo
+    axios
+      .delete(`https://jsonplaceholder.typicode.com/todos/${todoId}`)
+      .then(() => {
+        const updatedTodos = todos.filter((todo) => todo.id !== todoId); // Remove the deleted todo from the todos state
+        setTodos(updatedTodos); // Update the todos state
+        setPendingTasks(updatedTodos.filter((todo) => !todo.completed).length); // Update the number of pending tasks
+      })
+      .catch((error) => console.log(error));
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        marginTop: "60px",
+        marginBottom: "80px",
+      }}
+    >
+      <Paper elevation={3} sx={{ p: 2 }}>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            marginBottom: "30px",
+          }}
+        >
+          <Paper
+            component="form"
+            sx={{
+              p: "2px 4px",
+              display: "flex",
+              alignItems: "center",
+              width: "100%",
+            }}
+          >
+            <InputBase
+              sx={{ ml: 2, flex: 1 }}
+              placeholder="Write your Todos here..."
+              inputProps={{ "aria-label": "search google maps" }}
+              value={newTodo}
+              onChange={handleInputChange}
+              fullWidth
+            />
+            <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+            <IconButton
+              color="primary"
+              sx={{ p: "10px" }}
+              aria-label="directions"
+              onClick={handleAddTodo}
+            >
+              <AddIcon />
+            </IconButton>
+          </Paper>
+        </Box>
+
+        {todos.map((todo) => (
+          <TodoItem
+            key={todo.id}
+            todo={todo}
+            handleCheckboxChange={handleCheckboxChange}
+            handleDeleteTodo={handleDeleteTodo}
+            handleEditTodo={handleEditTodo}
+          />
+        ))}
+        <Divider sx={{ my: 2 }} />
+        <Typography variant="body1" component="p">
+          {pendingTasks} item(s) left
+        </Typography>
+      </Paper>
+    </div>
+  );
+};
+
+export default Todo;
